616be8882197bf46c36b0a85cfcc925217f71641
JB <jbotto94@users.noreply.github.com>
2019-09-18 19:21:35 +0100

    Fixing minor error

--- Tests/FableAssertEquality.Tests/DeepEqual.js
+++ Tests/FableAssertEquality.Tests/DeepEqual.js
@@ -3,5 +3,6 @@ module.exports = {
     DeepEq: function (objA, objB ) {
         const assert = require('assert');
         console.log(objA + " " + objB);
-        assert.strictEqual(objA, objB);
+        assert.deepEqual(objA, objB);
+    }
 }
